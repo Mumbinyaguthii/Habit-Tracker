1c811961321ed8718631843d80d0f697846fded5
mumbinyaguthii <janenyaguthii931@gmail.com>
2026-08-30 19:56:44 +0300

    added a functioning edit button

--- src/components/HabitItem.jsx
+++ src/components/HabitItem.jsx
@@ -1,4 +1,9 @@
+import { useState } from "react";
+
 const HabitItem = ({ habit, toggleHabit, deleteHabit, calculateStreak }) => {
+  const [isEditting, setIsEditting] = useState(false);
+  const [editedName, setEditedName] = useState(habit.name);
+
   return (
     <div className="habit-item" key={habit.id}>
       <input
@@ -6,13 +11,24 @@ const HabitItem = ({ habit, toggleHabit, deleteHabit, calculateStreak }) => {
         checked={habit.completed}
         onChange={() => toggleHabit(habit.id)}
       />
-      <span>{habit.name}</span>
-      <span>{habit.category}</span>
+      {isEditting ? (
+        <>
+          <input
+            type="text"
+            Value={editedName}
+            onChange={(e) => setEditedName(e.target.value)}
+          />
+          <button>Save</button>
+        </>
+      ) : (
+        <span>{habit.name}</span>
+      )}
+      ;<span>{habit.category}</span>
       <p>🔥 {calculateStreak(habit.completedDates)} day streak</p>
       <p>{habit.completedDates.join(", ")}</p>
-
-      <button className="edit">Edit</button>
-
+      <button className="edit" onClick={() => setIsEditting(true)}>
+        Edit
+      </button>
       <button className="del" onClick={() => deleteHabit(habit.id)}>
         Delete
       </button>
